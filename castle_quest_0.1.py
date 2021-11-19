ff91e1d5d161d8763a631bc1ae306d6c40fcfc1a
Ferdinand Vanderborght <ferdivdb@Ferdinands-MacBook-Air.local>
2021-11-19 18:58:54 +0100

    update code brouillon

--- castle_quest_0.1.py
+++ castle_quest_0.1.py
@@ -104,10 +104,6 @@ def read_matrice(file):
 		Elle ouvre ce fichier et renvoie en sortie une matrice.
 
 	'''
-	f = open(file, 'r')
-	matrice = [[int(n) for n in line.split(' ')] for line in f ]
-	matrice.reverse()
-	return matrice
 
 
 
@@ -120,14 +116,7 @@ def calcul_step(matrice):
 		de cases qu'elle doit accueillir, retenir la plus faible de ces deux valeurs
 
 	'''
-	x = len(matrice[0])
-	y = len(matrice)
-	l = x*x
-	h = y*x
-	surface_pixel = l*h
-	case = x*y
-	step = (surface_pixel/case)/x
-	return case, step
+
 
 def coordonnees(case, step):
 	'''
@@ -138,14 +127,7 @@ def coordonnees(case, step):
 		des coordonnées verticales va de bas en haut. 
 
 	'''
-	matrice = read_matrice('plan_chateau.txt')
-	pen = turtle.Turtle()
-	sc = turtle.Screen()
-	sc.setup(len(matrice[0])*len(matrice[0]), len(matrice)*len(matrice[0]))
-	TURTLE_SIZE = len(matrice[0])
-	pen.goto(TURTLE_SIZE/2 - sc.window_width()/2, -(sc.window_height()/2 - TURTLE_SIZE/2)+step[1])
-	case = pen.pos()
-	return case
+
 	
 
 def trace_carre(dimention):
@@ -154,12 +136,6 @@ def trace_carre(dimention):
 		Trace un carré dont la dimension en pixels turtle est donnée en argument.
 
 	'''
-	pen.color="white"
-	for i in range(4):
-		pen.pendown()
-		pen.forward(dimention)
-		pen.left(90)
-		pen.penup()
 
 
 def tracer_case(case, color, step):
@@ -172,12 +148,7 @@ def tracer_case(case, color, step):
 
 
 	'''
-	pen.begin_fill()
-	trace_square(case)
-	pen.penup()
-	pen.forward(step)
-	pen.pendown()
-	pen.end_fill()
+
 	
 
 
@@ -193,30 +164,9 @@ def show_plan(matrice):
 		dit la matrice.
 
 	'''
-	step = calcul_step(matrice)
-	pos = coordonnees(calcul_step(matrice), step)
-	pos1 = pos[0]
-	pos2 = pos[1]
-	color = 'white'
-	for i in range(len(matrice)):
-		pos2 += step[1]
-		for j in range(len(matrice[0])):
-			if matrice[i][j] == 3:
-				color = 'orange'
-			elif matrice[i][j] == 1:
-				color = 'azure4'
-			elif matrice[i][j] == 4:
-				color = 'green'
-			else:
-				color = 'white'
-			trace_case(coordonnees(step[0], step[1]), color, step[1])
-		pen.penup()
-		pen.goto(pos1, pos2)
-		pen.pendown()	
-	turtle.done()
 
 
-show_plan(read_matrice('plan_chateau.txt'))
+
 
 	
 	
